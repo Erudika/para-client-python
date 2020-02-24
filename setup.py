0ae88b0feb2e2a0c08ee68dbde88a1d699bf76bb
Alex Bogdanovski <alex@erudika.com>
2020-02-25 00:04:45 +0200

    added license

--- setup.py
+++ setup.py
@@ -96,11 +96,11 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
+    license='Apache 2.0',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
